168e8ca709070cfdd88025b08d27b5cdc31b8309
hizubikigroup <107944353+hizubikigroup@users.noreply.github.com>
2022-06-25 23:17:47 -0500

    Add files via upload

---
+++ js/controller.js
@@ -0,0 +1,11 @@
+import Model from "./model.js";
+import View from "./view.js";
+
+export default class Controller {
+    view = new View()
+    model = new Model()
+
+    constructor() {
+        
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+let btnAdd = document.getElementById('btn_add')
+
+btnAdd.addEventListener('click', () => {
+    let modalDialog = document.querySelector('.modal__dialog')
+
+    if (true) {
+        modalDialog.style.animation = 'animationModal .7s'
+        modalDialog.style.opacity = '1'
+    }
+})
+
+
